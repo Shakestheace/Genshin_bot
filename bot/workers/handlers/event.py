aab82fbb73d629e5cdf44e0531381666d412b291
Nubuki-all <121189493+Nubuki-all@users.noreply.github.com>
2024-10-23 11:14:10 +0000

    pyLint: auto-fixes

--- bot/workers/handlers/event.py
+++ bot/workers/handlers/event.py
@@ -1,6 +1,11 @@
 from bot.config import conf
 from bot.utils.log_utils import logger
-from bot.utils.msg_utils import chat_is_allowed, construct_event, event_handler, mentioned
+from bot.utils.msg_utils import (
+    chat_is_allowed,
+    construct_event,
+    event_handler,
+    mentioned,
+)
 
 from .dev import eval_handler, getlogs
 from .manage import pause_handler, restart_handler, update_handler
