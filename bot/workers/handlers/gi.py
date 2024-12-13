6ea933ef8d907aeb092e6101f0dbf333e5a54754
Nubuki-all <121189493+Nubuki-all@users.noreply.github.com>
2024-12-13 03:29:01 +0100

    update gi.py

--- bot/workers/handlers/gi.py
+++ bot/workers/handlers/gi.py
@@ -90,7 +90,7 @@ async def enka_handler(event, args, client):
                 return await u_reply.edit("Updated enka assets.")
             await u_reply.delete()
         if not (card or cards or dump or prof):
-            return await event.reply(f"***{enka_handler.__doc__}***")
+            return await event.reply(f"```{enka_handler.__doc__}```")
         if arg.t not in ("1", "2"):
             arg.t = 1
         profile, error = await get_enka_profile(args)
