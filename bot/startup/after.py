f42841dcebf024517d9de358adafc5a8dd1305c4
Nubuki-all <121189493+Nubuki-all@users.noreply.github.com>
2024-12-13 02:24:43 +0100

    update after.py

--- bot/startup/after.py
+++ bot/startup/after.py
@@ -75,7 +75,7 @@ async def on_termination():
 
 async def wait_on_client():
     while True:
-        if bot.client.is_logged_in()
+        if bot.client.is_logged_in():
             if not file_exists(con_ind):
                 touch(con_ind)
             break
